6cd825b1b09113918f89533ef6c1ae82f06685d6
TOSHI <tomoki.ohnishi@fuji.waseda.jp>
2025-12-26 17:20:22 +0900

    style -> styles

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     lib: {
       entry: {
         index: "src/index.ts",
-        style: "src/style.css",
+        style: "src/styles.css",
       },
       formats: ["es"],
       fileName: (_, name) => `${name}.js`,
