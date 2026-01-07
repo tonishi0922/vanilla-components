a667f4a294ac48d5a79ab2ed0faa900643a69ce0
TOSHI <tomoki.ohnishi@fuji.waseda.jp>
2026-01-07 10:27:52 +0900

    storybookのnpm install

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,50 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   build: {
     cssCodeSplit: true,
     lib: {
       entry: {
         index: "src/index.ts",
-        styles: "src/styles.css",
+        styles: "src/styles.css"
       },
       formats: ["es"],
-      fileName: (_, name) => `${name}.js`,
+      fileName: (_, name) => `${name}.js`
     },
     rollupOptions: {
       output: {
-        assetFileNames: "[name][extname]",
-      },
-    },
+        assetFileNames: "[name][extname]"
+      }
+    }
   },
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
+  }
 });
